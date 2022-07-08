7194ac3acdc26754478aaf5fc1ab3b8ea100d482
benh37 <ben.harveyy33@gmail.com>
2022-07-08 12:44:50 +0200

    checkpoint 1 commit

--- App.js
+++ App.js
@@ -1,28 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import CourseList from './components/CourseList';
 
 const schedule = {
-  title: "CS Courses for 2018-2019"
+  "title": "CS Courses for 2018-2019",
+  "courses": [
+    {
+      "id": "F101",
+      "title": "Computer Science: Concepts, Philosophy, and Connections",
+      "meets": "MWF 11:00-11:50"
+    },
+    {
+      "id": "F110",
+      "title": "Intro Programming for non-majors",
+      "meets": "MWF 10:00-10:50"
+    },
+    {
+      "id": "F111",
+      "title": "Fundamentals of Computer Programming I",
+      "meets": "MWF 13:00-13:50"
+    },
+    {
+      "id": "F211",
+      "title": "Fundamentals of Computer Programming II",
+      "meets": "TuTh 12:30-13:50"
+    }
+  ]
 };
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.bannerStyle}>{schedule.title}</Text>
-    </View>
-  );
-}
+const Banner = ({ title }) => (
+  <Text style={styles.banner}>{ title }</Text>
+);
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 20,
   },
-  bannerStyle: {
+  banner: {
     color: '#888',
     fontSize: 32,
-  }
+  },
 });
 
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
+    </SafeAreaView>
+  );
+};
+  
 export default App;
