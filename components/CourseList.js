2dd9ceb80e2075f66300b90a9d65e773d1adb856
benh37 <ben.harveyy33@gmail.com>
2022-07-14 11:16:53 +0200

    checkpoint 2

--- components/CourseList.js
+++ components/CourseList.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, ScrollView} from 'react-native';
-import Course from './Course';
+import CourseSelector from './CourseSelector'
+import TermSelector from './TermSelector';
 
-const CourseList = ({ courses }) => (
+const termMap = { F: 'Fall', W: 'Winter', S: 'Spring'};
+const terms = Object.values(termMap);
+
+const getCourseTerm = course => (
+  termMap[course.id.charAt(0)]
+);
+
+const CourseList = ({courses, view}) => {
+  const [selectedTerm, setSelectedTerm] = useState('Fall');
+  const termCourses = courses.filter(course => selectedTerm === getCourseTerm(course));
+  
+  return (
     <ScrollView>
-      <View style={styles.courseList}>
-        { courses.map(course => <Course key={course.id} course={ course } />) }
-      </View>
+      <TermSelector terms={terms} selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
+      <CourseSelector courses={termCourses} view={view}/>
     </ScrollView>
   );
-const styles = StyleSheet.create({
-    courseList: {
-        flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-      },
-    });
+};
+
+
+
 export default CourseList;
